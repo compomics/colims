9ee390d2ea48bf674e2909ac520be18b413740d8
Niels Hulstaert <nielshulstaert@gmail.com>
2013-11-28 14:28:37 +0100

    added FastaDb class to colims schema

---
+++ colims-model/src/main/java/com/compomics/colims/model/FastaDb.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.compomics.colims.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Niels Hulstaert
+ */
+@Table(name = "search_engine")
+@Entity
+public class SearchEngine extends AbstractDatabaseEntity {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @OneToMany(mappedBy = "searchEngine")
+    private List<SearchAndValSetHasSearchEngine> searchAndValSetHasSearchEngines = new ArrayList<>();  
+
+    public List<SearchAndValSetHasSearchEngine> getSearchAndValSetHasSearchEngines() {
+        return searchAndValSetHasSearchEngines;
+    }
+
+    public void setSearchAndValSetHasSearchEngines(List<SearchAndValSetHasSearchEngine> searchAndValSetHasSearchEngines) {
+        this.searchAndValSetHasSearchEngines = searchAndValSetHasSearchEngines;
+    }
+}
