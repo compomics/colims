b3d3d3e431cbaa14957b8a4804a57a1dedeba33b
Niels Hulstaert <nielshulstaert@gmail.com>
2014-11-07 12:03:14 +0100

    Javadoc additions.

--- colims-core/src/main/java/com/compomics/colims/core/io/DataImporter.java
+++ colims-core/src/main/java/com/compomics/colims/core/io/DataImporter.java
@@ -14,39 +14,39 @@ public interface DataImporter {
     /**
      * Init the import.
      *
-     * @param dataImport
+     * @param dataImport the DataImport instance
      */
     void initImport(DataImport dataImport);
-    
+
     /**
      * Clear resources used during the import.
-     * 
+     *
      */
     void clear();
 
     /**
      * Import the SearchAndValidationSettings.
      *
-     * @return
-     * @throws com.compomics.colims.core.io.MappingException
+     * @return the mapped SearchAndValidationSettings instance
+     * @throws MappingException thrown in case of an error during the mapping
      */
     SearchAndValidationSettings importSearchSettings() throws MappingException;
 
     /**
      * Import the QuantificationSettings.
      *
-     * @return
-     * @throws com.compomics.colims.core.io.MappingException
+     * @return the mapped QuantificationSettings instance
+     * @throws MappingException thrown in case of an error during the mapping
      */
     QuantificationSettings importQuantSettings() throws MappingException;
 
     /**
      * Import the search input and identification results.
      *
-     * @param searchAndValidationSettings
-     * @param quantificationSettings
-     * @return
-     * @throws com.compomics.colims.core.io.MappingException
+     * @param searchAndValidationSettings the SearchAndValidationSettings instance
+     * @param quantificationSettings the QuantificationSettings instance
+     * @return the list of analytical runs
+     * @throws MappingException thrown in case of an error during the mapping
      */
     List<AnalyticalRun> importInputAndResults(SearchAndValidationSettings searchAndValidationSettings, QuantificationSettings quantificationSettings) throws MappingException;
 
