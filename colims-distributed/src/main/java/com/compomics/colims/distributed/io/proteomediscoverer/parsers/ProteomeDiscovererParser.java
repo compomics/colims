1c7b7f1843177faafdd74a240dbfc7630d5af9e4
DavyMaddelein <davy.maddelein@gmail.com>
2016-01-23 13:21:43 +0100

    added thermo-msf-parser-API depdendency set up to start writing parsing of proteome discoverer databases

--- colims-distributed/src/main/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParser.java
+++ colims-distributed/src/main/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParser.java
@@ -1,7 +1,12 @@
 package com.compomics.colims.distributed.io.proteomediscoverer.parsers;
 
+import com.compomics.colims.model.FastaDb;
+import com.compomics.colims.model.enums.FastaDbType;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.EnumMap;
 
 /**
  * Created by Davy Maddelein on 23/01/2016.
@@ -13,5 +18,14 @@ public class ProteomeDiscovererParser {
     private static final Logger LOGGER = Logger.getLogger(ProteomeDiscovererParser.class);
 
 
-    
+    public boolean parseDatabase(File ProteomeDiscovererDatabase, EnumMap<FastaDbType, FastaDb> fastaDbs) {
+        // step 0: preparations to start parsing
+
+
+        // step 1: retrieve analytical runs
+
+
+
+        return true;
+    }
 }
--- colims-distributed/src/test/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParserTest.java
+++ colims-distributed/src/test/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParserTest.java
@@ -1,9 +1,18 @@
 package com.compomics.colims.distributed.io.proteomediscoverer.parsers;
 
+import com.compomics.colims.core.io.MappingException;
+import com.compomics.colims.distributed.io.maxquant.UnparseableException;
+import com.compomics.colims.model.FastaDb;
+import com.compomics.colims.model.enums.FastaDbType;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.EnumMap;
 
 /**
  * Created by Davy Maddelein on 23/01/2016.
@@ -17,5 +26,17 @@ public class ProteomeDiscovererParserTest {
     @Autowired
     ProteomeDiscovererParser proteomeDiscovererParser;
 
+
+
+        @Before
+        public void setUp() throws MappingException, UnparseableException, IOException {
+            FastaDb proteomeDiscovererFastaDb = new FastaDb();
+
+            EnumMap<FastaDbType, FastaDb> fastaDbs = new EnumMap<>(FastaDbType.class);
+            fastaDbs.put(FastaDbType.PRIMARY, proteomeDiscovererFastaDb);
+
+
+
+        }
 
 }
