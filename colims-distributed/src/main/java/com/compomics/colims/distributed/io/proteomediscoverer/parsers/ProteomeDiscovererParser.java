1dc770d73db877565caa6aa46de17ad70b4f3752
DavyMaddelein <davy.maddelein@gmail.com>
2016-01-25 12:40:29 +0100

    ready for parsing

--- colims-distributed/src/main/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParser.java
+++ colims-distributed/src/main/java/com/compomics/colims/distributed/io/proteomediscoverer/parsers/ProteomeDiscovererParser.java
@@ -2,10 +2,12 @@ package com.compomics.colims.distributed.io.proteomediscoverer.parsers;
 
 import com.compomics.colims.model.FastaDb;
 import com.compomics.colims.model.enums.FastaDbType;
+import com.compomics.thermo_msf_parser_API.lowmeminstance.model.MsfFile;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.sql.SQLException;
 import java.util.EnumMap;
 
 /**
@@ -18,9 +20,12 @@ public class ProteomeDiscovererParser {
     private static final Logger LOGGER = Logger.getLogger(ProteomeDiscovererParser.class);
 
 
-    public boolean parseDatabase(File ProteomeDiscovererDatabase, EnumMap<FastaDbType, FastaDb> fastaDbs) {
+    public boolean parseDatabase(File ProteomeDiscovererDatabase, EnumMap<FastaDbType, FastaDb> fastaDbs) throws SQLException, ClassNotFoundException {
         // step 0: preparations to start parsing
 
+            MsfFile dbToParse = new MsfFile(ProteomeDiscovererDatabase);
+
+            dbToParse.getVersion();
 
         // step 1: retrieve analytical runs
 
