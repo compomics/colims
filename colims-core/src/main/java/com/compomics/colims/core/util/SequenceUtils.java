37141a33b28b27457aea86e1c0f20c900c747247
Niels Hulstaert <nielshulstaert@gmail.com>
2017-01-21 22:01:17 +0100

    refactoring of sequence utils

--- colims-core/src/main/java/com/compomics/colims/core/util/SequenceUtils.java
+++ colims-core/src/main/java/com/compomics/colims/core/util/SequenceUtils.java
@@ -4,11 +4,7 @@ import com.compomics.util.experiment.biology.AminoAcidPattern;
 import com.compomics.util.experiment.biology.AminoAcidSequence;
 import com.compomics.util.preferences.SequenceMatchingPreferences;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
 /**
  * This utility class provides convenience methods for retrieving sequence
@@ -37,7 +33,7 @@ public class SequenceUtils {
      *
      * @param proteinSequence the protein sequence
      * @param peptideSequence the peptide sequence
-     * @return the list of PeptidePosition instance
+     * @return the list of PeptidePosition instances, the positions are one based
      * @throws IllegalStateException error thrown if the peptide sequence could not be found in the protein sequence
      */
     public static List<PeptidePosition> getPeptidePositions(String proteinSequence, String peptideSequence) {
@@ -73,7 +69,7 @@ public class SequenceUtils {
 
     /**
      * Get the peptide start indexes in the given protein sequence. If the peptide occurs more than once, multiple start
-     * positions are returned.
+     * positions are returned. One based.
      *
      * @param proteinSequence the protein sequence
      * @param peptideSequence the peptide sequence
@@ -91,7 +87,7 @@ public class SequenceUtils {
      *
      * @param proteinSequence the protein sequence
      * @param peptideSequence the sequence of the peptide of interest
-     * @param startPosition   the peptide start position in the protein
+     * @param startPosition   the peptide start position in the protein, one based
      * @return the amino acids surrounding a peptide in the protein sequence
      */
     public static Character[] getSurroundingAAs(String proteinSequence, String peptideSequence, int startPosition) {
@@ -115,163 +111,68 @@ public class SequenceUtils {
     /**
      * Calculate protein coverage by using protein sequence and peptide sequence.
      *
-     * @param proteinSequence the protein sequence
-     * @param peptides        the list op peptide sequences
+     * @param proteinSequence  the protein sequence
+     * @param peptideSequences the list op peptide sequences
      * @return the protein sequence coverage
      */
-    public static double calculateProteinCoverage(String proteinSequence, List<String> peptides) {
-        String mappedProtein = mapPeptideSequences(proteinSequence, peptides);
+    public static double calculateProteinCoverage(String proteinSequence, List<String> peptideSequences) {
+        TreeMap<Integer, Boolean> coverageAnnotations = mapPeptideSequences(proteinSequence, peptideSequences);
 
-        String mergedPeptideAnnotations = mergePeptideAnnotations(mappedProtein);
-
-        double mergedPeptideSize = 0.0;
-
-        Pattern p = Pattern.compile(Pattern.quote("[") + "(.*?)" + Pattern.quote("]"));
-
-        Matcher m = p.matcher(mergedPeptideAnnotations);
-
-        while (m.find()) {
-            mergedPeptideSize += m.group(1).length();
+        int coveredLength = 0;
+        int tempLength = 0;
+        for (Map.Entry<Integer, Boolean> entry : coverageAnnotations.entrySet()) {
+            if (entry.getValue()) {
+                tempLength = entry.getKey();
+            } else {
+                coveredLength += entry.getKey() - tempLength + 1;
+            }
         }
 
-        double proteinSize = proteinSequence.length();
-
-        double coverage = mergedPeptideSize / proteinSize;
-
-        return coverage;
+        return (double) coveredLength / proteinSequence.length();
     }
 
     /**
-     * Maps a group of peptides to a protein string.
+     * Maps a list of peptide sequences onto a protein sequence.
      *
      * @param protein  the protein to match
      * @param peptides the peptides to match
-     * @return the annotated protein sequence where [ denotes the start of a peptide and ] the end of a peptide
-     * @throws IllegalArgumentException if a peptide did not belong to the protein
+     * @return the map with peptide annotations (key: AA index (null based); value: true for start of covered area,
+     * false for end)
+     * @throws IllegalArgumentException if a peptide sequence doesn't occur on the protein
      */
-    private static String mapPeptideSequences(String protein, List<String> peptides) throws IllegalArgumentException {
-        return mapPeptideSequences(protein, '[', ']', peptides);
-    }
+    public static TreeMap<Integer, Boolean> mapPeptideSequences(String protein, List<String> peptides) throws IllegalArgumentException {
+        TreeMap<Integer, Boolean> coverageAnnotations = new TreeMap<>();
 
-    /**
-     * Maps a group of peptides to a protein string denoted by the passed
-     * delimiters.
-     *
-     * @param protein  the protein to match
-     * @param opener   the starting delimiter for a peptide
-     * @param closer   the closing delimiter for a peptide
-     * @param peptides the peptides to match
-     * @return the annotated protein sequence where peptides are denoted by the passed delimiters
-     * @throws IllegalArgumentException if a peptide did not belong to the protein
-     */
-    private static String mapPeptideSequences(String protein, Character opener, Character closer, List<String> peptides) throws IllegalArgumentException {
-        StringBuilder builder = new StringBuilder(protein);
-        String holder = protein.toUpperCase(Locale.US);
-        for (String aPeptide : peptides) {
-            if (holder.contains(aPeptide.toUpperCase(Locale.US))) {
-                int indexOfPeptide = holder.indexOf(aPeptide);
-                String subsequenceHolder = holder.subSequence(0, indexOfPeptide).toString();
-                String subsequenceBuilder = builder.subSequence(0, indexOfPeptide).toString().replace("[", "").replace("]", "");
-                while (!subsequenceBuilder.equals(subsequenceHolder)) {
-                    indexOfPeptide++;
-                    subsequenceBuilder = builder.subSequence(0, indexOfPeptide).toString().replace("[", "").replace("]", "");
+        for (String peptide : peptides) {
+            List<PeptidePosition> peptidePositions = getPeptidePositions(protein, peptide);
+            for (PeptidePosition peptidePosition : peptidePositions) {
+                //get the annotations preceding the peptide start position
+                SortedMap<Integer, Boolean> headMap = coverageAnnotations.headMap(peptidePosition.getStartPosition());
+                if (headMap.isEmpty() || !headMap.get(headMap.lastKey())) {
+                    coverageAnnotations.put(peptidePosition.getStartPosition() - 1, true);
                 }
-                long count = builder.subSequence(0, indexOfPeptide).chars().filter(e -> e == '[' || e == ']').count();
-                builder.insert((int) (holder.indexOf(aPeptide.toUpperCase(Locale.US)) + count), '[');
-                count += builder.subSequence(indexOfPeptide + 1, indexOfPeptide + 1 + aPeptide.length()).chars().filter(e -> e == '[' || e == ']').count();
-                builder.insert((int) (holder.indexOf(aPeptide.toUpperCase(Locale.US)) + 1 + count + aPeptide.length()), ']');
 
-            } else {
-                throw new IllegalArgumentException("peptide " + aPeptide + " was not found in the protein sequence");
+                //get the annotations starting from the peptide start position
+                SortedMap<Integer, Boolean> tailMap = coverageAnnotations.tailMap(peptidePosition.getStartPosition() + 1);
+                if (tailMap.isEmpty()) {
+                    coverageAnnotations.put(peptidePosition.getEndPosition() - 1, false);
+                } else {
+                    //iterate over the entries
+                    for (Map.Entry<Integer, Boolean> entry : new TreeMap<>(tailMap).entrySet()) {
+                        if (entry.getKey() < peptidePosition.getEndPosition()) {
+                            coverageAnnotations.remove(entry.getKey());
+                        } else if (entry.getValue()) {
+                            coverageAnnotations.put(peptidePosition.getEndPosition() - 1, false);
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
             }
         }
-        return builder.toString();
-    }
 
-    /**
-     * Merges overlapping peptide annotations in a String.
-     *
-     * @param mappedString the string to merge, assumes delimiters are [ for start and ] for end
-     * @return the merged string
-     */
-    private static String mergePeptideAnnotations(String mappedString) {
-        return mergePeptideAnnotations(mappedString, '[', ']');
-    }
-
-    /**
-     * Merges overlapping peptide annotations in a String.
-     *
-     * @param mappedString the string to merge
-     * @param opener       the opening delimiter for a peptide
-     * @param closer       the closing delimiter for a peptide
-     * @return the merged string
-     */
-    private static String mergePeptideAnnotations(String mappedString, Character opener, Character closer) {
-        int openframe = 0;
-        StringBuilder builder = new StringBuilder(mappedString);
-
-        for (int i = 0; i < builder.length(); i++) {
-            if (openframe > 0 && builder.charAt(i) == opener) {
-                builder.deleteCharAt(i);
-                i -= 1;
-                openframe += 1;
-            } else if (openframe > 1 && builder.charAt(i) == closer) {
-                builder.deleteCharAt(i);
-                i -= 1;
-                openframe -= 1;
-                //combine both situations in 1
-            } else if (openframe == 1 && builder.charAt(i) == closer) {
-                openframe -= 1;
-            } else if (builder.charAt(i) == opener) {
-                openframe += 1;
-            }
-        }
-        //if openframe > 0 something went wrong
-        return builder.toString();
-    }
-
-    /**
-     * Find Amino acid preceding the peptide in the protein sequence.
-     *
-     * @param protein
-     * @param peptide
-     * @return amino acid if peptide exists in given protein sequence.
-     */
-    public static String findAminoAcidPrecedingPeptide(String protein, String peptide) {
-        protein = protein.toUpperCase(Locale.US);
-        peptide = peptide.toUpperCase(Locale.US);
-        if (protein.contains(peptide)) {
-            // if peptide is N-terminal returns "-"
-            if (protein.indexOf(peptide) > 0) {
-                return String.valueOf(protein.charAt(protein.indexOf(peptide) - 1));
-            } else {
-                return "-";
-            }
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * Find Amino acid following the peptide in the protein sequence.
-     *
-     * @param protein
-     * @param peptide
-     * @return amino acid if peptide exists in given protein sequence.
-     */
-    public static String findAminoAcidFollowingPeptide(String protein, String peptide) {
-        protein = protein.toUpperCase(Locale.US);
-        peptide = peptide.toUpperCase(Locale.US);
-        if (protein.contains(peptide)) {
-            // if peptide is C-terminal returns "-"
-            if (protein.indexOf(peptide) + peptide.length() < protein.length()) {
-                return String.valueOf(protein.charAt(protein.indexOf(peptide) + peptide.length()));
-            } else {
-                return "-";
-            }
-        } else {
-            return null;
-        }
+        return coverageAnnotations;
     }
 
     /**
